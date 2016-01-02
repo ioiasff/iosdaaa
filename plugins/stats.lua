3d99a98a44e0498bc7a95a397333bf11e2f2ea3d
Arashbrsh <r3v3ng3r1111@gmail.com>
2016-01-02 20:57:43 +0330

    Create stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -138,10 +138,10 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teledeath)",-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (teledeath)",-- Put everything you like :)
 		"^[!/]([Tt]eledeath)"-- Put everything you like :)
     }, 
   run = run
